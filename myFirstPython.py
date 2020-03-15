15a8ff9ed6751bb0dbac6794932a7968a220dd76
jambompeople <2572432388@qq.com>
2020-03-15 19:23:18 -0400

    ignore the classes, and write some thing from the book I read

--- myFirstPython.py
+++ myFirstPython.py
@@ -1,2 +1,4 @@
-print("je suis jackson")
-print("hero")
+brand = 'apple'
+exchangeRate = 1.235235245
+message = 'the price of this {0:s} laptop is {1:d} USD and the exchange rate is {2:4.2f} USD to 1 EUR we also offer {3:s}'.format(brand,1229,exchangeRate,'banana')
+print(message)

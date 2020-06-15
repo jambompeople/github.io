2c058c989149543b6f1a8531d6c5d0846f245b71
jambompeople <you@example.com>
2020-06-15 15:09:30 -0400

    learning numpy

---
+++ hi.py
@@ -0,0 +1,28 @@
+
+import numpy as np
+
+hi = np.array([1, 2, 3, 4, 5])
+
+print(hi)
+
+print(type(hi))
+
+hello = np.array((1,2,4))
+
+print(hello)
+
+dimend = np.array([[1,2,3,4],[3,4,5,6,7]])
+
+print(dimend)
+
+print(dimend.ndim)
+
+arra = np.array([1,2,3,4], ndmin = 5)
+
+print(arra)
+
+print(arra.ndim)
+
+list = np.array([1,5,6,7,7])
+
+print(list[0])

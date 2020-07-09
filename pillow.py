af67f0088d3eef9c0c3cedb88e2924bf45f00dd6
jambompeople <you@example.com>
2020-07-09 14:09:10 -0400

    trying to learn pillow but I am very confused

--- pillow.py
+++ pillow.py
@@ -1,3 +1,7 @@
 from PIL import Image
-im = Image.open("index.jpeg")
-im.rotate(45).show()
+im = Image.open("/home/jambompeople/Downloads/index.jpeg").convert("LA")
+imgsmall = im.resize((16,16),resample = Image.BILINEAR)
+pixels = imgsmall.load()
+pixels[0,0] = (255, 0, 0)
+result = imgsmall.resize(im.size, Image.NEAREST)
+result.show()

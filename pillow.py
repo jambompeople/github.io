68d1a2464e78908273ea98dc2031ad7a4cbd204f
jambompeople <you@example.com>
2020-07-10 13:59:43 -0400

    I created a German flag

--- pillow.py
+++ pillow.py
@@ -1,7 +1,18 @@
 from PIL import Image
-im = Image.open("/home/jambompeople/Downloads/index.jpeg").convert("LA")
-imgsmall = im.resize((16,16),resample = Image.BILINEAR)
-pixels = imgsmall.load()
-pixels[0,0] = (255, 0, 0)
-result = imgsmall.resize(im.size, Image.NEAREST)
-result.show()
+im = Image.open("/home/jambompeople/Downloads/index.jpeg")
+im2 = im.resize((18,18), resample = Image.BILINEAR)
+pixel = im2.load()
+for i in range(0,18):
+    for j in range(0, 6):
+        pixel[i, j] = (0, 0, 0)
+
+for i in range(0,18):
+    for j in range(6, 12):
+        pixel[i, j] = (255, 0, 0)
+
+for i in range(0,18):
+    for j in range(12, 18):
+        pixel[i, j] = (255, 255, 0)
+
+im3 = im2.resize(im.size, Image.NEAREST)
+im3.show()
